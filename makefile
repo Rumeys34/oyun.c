e2b97448af91d3d8686d12607594381ee20edfc0
Rümeysa Ertekin <rumeysaertekin21@icloud.com>
2026-05-02 15:18:46 +0300

    arkaplan ve ana gemi resmi ekledin.Gemi hareket fonksiyonlarını ekleyip makefile güncelledim

--- makefile
+++ makefile
@@ -1,7 +1,24 @@
 
+CC = gcc
+# Mac Homebrew yollarını doğrudan ekliyoruz
+CFLAGS = -Wall -Wextra -std=c11 $(shell sdl2-config --cflags) -I/opt/homebrew/include
+LIBS = $(shell sdl2-config --libs) -lSDL2_image -lSDL2_mixer -L/opt/homebrew/lib
 
-all:
-	gcc oyun.c -o space -I/opt/homebrew/include -L/opt/homebrew/lib -lSDL2
+SRC = oyun.c
+OBJ = $(SRC:.c=.o)
+EXE = uzay_oyunu
 
-run:
-	./space
+all: $(EXE)
+
+$(EXE): $(OBJ)
+	$(CC) $(OBJ) -o $(EXE) $(LIBS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ) $(EXE)
+
+run: all
+	./$(EXE)
+
--- oyun.c
+++ oyun.c
@@ -1,16 +1,53 @@
+
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include <stdlib.h>
 #include <stdio.h>
 
+
+
+
+
+
+typedef struct {
+    float x, y;
+    int genislik, yukseklik;
+    float hız;
+    SDL_Texture* Gemi;
+
+} gemi;
+SDL_Texture* arkaPlan=NULL;
+
+
 int main(int argc, char* argv[]) {
+    IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
      if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Hata: %s\n", SDL_GetError());
         return 1;
     }
+
    
 
     SDL_Window* ekran= SDL_CreateWindow("oyun ekranı ", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
     SDL_Renderer* çizici = SDL_CreateRenderer(ekran, -1, SDL_RENDERER_ACCELERATED);
+    
    
+
+   
+    gemi anagemi;
+    anagemi.x = 400;
+    anagemi.y = 530;
+    anagemi.genislik = 70;
+    anagemi.yukseklik = 70;
+    anagemi.hız =10;
+    anagemi.Gemi = IMG_LoadTexture(çizici, "anagemi.png");
+    arkaPlan = IMG_LoadTexture(çizici, "arkplan2.jpg");
+    
+    
+
+
+
     int oyunDevamEdiyor = 1;
     SDL_Event olay;
             
@@ -20,17 +57,62 @@ int main(int argc, char* argv[]) {
             if (olay.type == SDL_QUIT) {
                 oyunDevamEdiyor = 0;
             }
+
+        
+
+               if (olay.type == SDL_KEYDOWN) {
+                    switch (olay.key.keysym.sym) {
+                       case SDLK_LEFT:
+                            anagemi.x -= anagemi.hız;
+                            break;
+                        case SDLK_RIGHT:
+                            anagemi.x += anagemi.hız;
+                            break;
+                        default:
+                            break;
+                    }
+                }
         }
-            
-    SDL_SetRenderDrawColor(çizici, 100, 250, 0, 0); 
-    SDL_RenderClear(çizici);
-    SDL_RenderPresent(çizici);
-    }
-      
+
+
+
+
+
+
+
+
+        
+         
+        SDL_SetRenderDrawColor(çizici, 0, 0, 0, 255);
+        SDL_RenderClear(çizici);
+
+        if(arkaPlan) {
+            SDL_RenderCopy(çizici, arkaPlan, NULL, NULL);
+        } 
+    
+
+    
+   
+    
+        SDL_Rect gemiögesi = { (int)anagemi.x, (int)anagemi.y, anagemi.genislik, anagemi.yukseklik };
+        if (anagemi.Gemi != NULL) {
+        SDL_RenderCopy(çizici, anagemi.Gemi, NULL, &gemiögesi);
+        } 
+        else {
+        SDL_SetRenderDrawColor(çizici, 255, 255, 255, 255);
+        SDL_RenderFillRect(çizici, &gemiögesi);
+        }
+
+        SDL_RenderPresent(çizici);
+    
+    } 
       
     SDL_DestroyRenderer(çizici);
     SDL_DestroyWindow(ekran);
+    IMG_Quit();
+    SDL_DestroyTexture(arkaPlan); 
     SDL_Quit();
+
 
     return 0;
 } 
